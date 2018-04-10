915d797f6467273f929f3c1b774a7fc23a3d0ce8
russell <russell@104.39.208.150>
2018-04-10 14:23:09 -0400

    Create initial project

--- src/App.java
+++ src/App.java
@@ -16,6 +16,7 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Qhhsodf");
         System.out.println("Bleee bloop blue blue blah");
+        System.out.println("I F**king hate this");
     }
     
 }
